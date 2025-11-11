a0265050476c94a83236a6e9bef38075874b225d
Deepak Kumar <deepakkumardash313@gmail.com>
2025-11-12 02:10:01 +0530

    Add : pinia and loading state to auth button

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -2,17 +2,23 @@
 import { createAuthClient } from "better-auth/client";
 
 const authClient = createAuthClient();
+const loading = ref(false);
 
 async function signIn() {
+  loading.value = true;
   await authClient.signIn.social({
     provider: "github",
     callbackURL: "/dashboard",
   });
+  loading.value = false;
 }
 </script>
 
 <template>
-  <button class="btn btn-accent" @click="signIn">
-    Continue with github <Icon name="tabler:brand-github" size="24" />
+  <button :disabled="loading" class="btn btn-accent" @click="signIn">
+    Continue with github
+
+    <span v-if="loading" class="loading loading-spinner loading-md" />
+    <Icon v-else name="tabler:brand-github" size="24" />
   </button>
 </template>
---
+++ app/pages/dashboard/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hero bg-base-200 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          GEOPULSE
+        </h1>
+        <p class="py-6">
+          A map based travel diary that lets you add photos as memories for the places you have travelled.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
