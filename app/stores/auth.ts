7f2bf1d450aa562f63ce79b81e08187c45287987
Deepak Kumar <deepakkumardash313@gmail.com>
2025-11-12 02:18:39 +0530

    Add : pinia and update login logic

---
+++ app/stores/auth.ts
@@ -0,0 +1,19 @@
+import { createAuthClient } from "better-auth/client";
+import { defineStore } from "pinia";
+
+const authClient = createAuthClient();
+
+export const useAuthStore = defineStore("useAuthStore", () => {
+  const loading = ref(false);
+
+  async function signIn() {
+    loading.value = true;
+    await authClient.signIn.social({
+      provider: "github",
+      callbackURL: "/dashboard",
+    });
+    loading.value = false;
+  }
+
+  return { loading, signIn };
+});
