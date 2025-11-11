64070f3a97cf0702420309ed108c72113461f327
Deepak Kumar <deepakkumardash313@gmail.com>
2025-11-12 02:36:09 +0530

    Add : error page and update env and gh action file

---
+++ app/pages/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const route = useRoute();
+const error = route.query.error || "Something went wrong";
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 text-center min-h-72 mx-auto flex flex-col justify-center items-center gap-4">
+    <div role="alert" class="alert alert-error min-w-1/2 text-center">
+      <span>Uh Ohh! {{ error }}. Please contact customer support</span>
+    </div>
+
+    <Nuxtlink to="/" class="btn btn-primary">
+      <Icon name="tabler:arrow-left" size="32" />
+      Home
+    </Nuxtlink>
+  </div>
+</template>
