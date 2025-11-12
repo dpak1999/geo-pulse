f7f74a682efd7ed1414aa12dfb102146f0a92308
Deepak Kumar <deepakkumardash313@gmail.com>
2025-11-12 18:17:21 +0530

    Add : user session in store and avatar ui

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -3,7 +3,21 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
+  <details v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <summary class="btn m-1">
+      <div v-if="authStore.user.image" class="avatar mr-2">
+        <div class="w-7 rounded-full">
+          <img :src="authStore.user.image" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </summary>
+    <ul class="menu dropdown-content bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li><a>Sign Out</a></li>
+    </ul>
+  </details>
+
+  <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn">
     Continue with github
 
     <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
